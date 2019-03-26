6de9902c2a1ca0050cf65c388edbe9fae869bc1e
Jasmin Grant <jazzy@Jasmins-MacBook-Pro.local>
2019-03-25 18:45:09 -0700

    conways_life_mycode

--- src/Life.elm
+++ src/Life.elm
@@ -13,15 +13,53 @@ type alias Board = Cell -> CellStatus
 -- current status.
 nextStatus : Int -> CellStatus -> CellStatus
 nextStatus numberOfLivingNeighbors currentStatus =
-  todo "Copy your Lab 2 Code Here!"
+  case currentStatus of
+        Alive ->
+            if numberOfLivingNeighbors < 2
+                then Dead
+                else if numberOfLivingNeighbors > 3
+                then Dead
+                else Alive
+        Dead -> 
+            if numberOfLivingNeighbors == 3
+             then Alive 
+             else Dead 
 
 -- calculates the number of living neighbors of a cell,
 -- given a board and a cell.
 livingNeighbors : Board -> Cell -> Int
-livingNeighbors currentBoard { x, y } =
-  todo "Copy your Lab 2 Code Here!"
+livingNeighbors currentBoard { x, y } = 0
+ -- let
+    --     allNeighbors : List Cell
+    --     allNeighbors = [ { x = x - 1, y = y + 1 },
+    --                { x = x, y = y + 1 },
+    --                { x = x + 1, y = y + 1 },
+    --                { x = x - 1, y = y },
+    --                { x = x + 1, y = y },
+    --                { x = x - 1, y = y - 1 },
+    --                { x = x, y = y - 1 },
+    --                { x = x + 1, y = y - 1}
+    --                      ]
+    
+    -- in
+
+    -- sumOfList
+  
 
 -- calculates the next board given the current board.
 nextBoard : Board -> Board
 nextBoard currentBoard =
-  todo "Copy your Lab 2 Code Here!"
+  let
+        newBoard : Board
+        newBoard (cell) =
+            let
+                currStat = currentBoard (cell)
+                currLivingNbrs = livingNeighbors (currentBoard) (cell)
+                newStatus = nextStatus (currLivingNbrs) (currStat)
+            in
+            newStatus
+
+       
+    in
+    newBoard
+    
